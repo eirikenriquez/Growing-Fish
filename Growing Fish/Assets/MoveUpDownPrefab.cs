8332b462650b67db8ec210dda9266bca0a343d6d
Eirik Enriquez <84115583+eirikenriquez@users.noreply.github.com>
2023-02-06 20:58:03 +1300

    updated bomb spawn

--- Growing Fish/Assets/MoveUpDownPrefab.cs
+++ Growing Fish/Assets/MoveUpDownPrefab.cs
@@ -6,8 +6,11 @@ public class MoveUpDownPrefab : MonoBehaviour
 {
     public GameObject prefab;
     public GameObject boxColliderObject;
-
-    public int numberOfBombs = 5;
+    private BoxCollider2D boxCollider;
+    private Bounds bounds;
+    public int numberOfBombs;
+    public int maxBombs;
+    private List<GameObject> existingBombs = new List<GameObject>();
     //public float spawnRadius = 5f;
 
 
@@ -18,14 +21,19 @@ public class MoveUpDownPrefab : MonoBehaviour
             Debug.LogError("boxColliderObject is not assigned");
             return;
         }
-        BoxCollider2D boxCollider = boxColliderObject.GetComponent<BoxCollider2D>();
+        boxCollider = boxColliderObject.GetComponent<BoxCollider2D>();
         if (boxCollider == null)
         {
             Debug.LogError("boxColliderObject does not have a BoxCollider2D component");
             return;
         }
-        Bounds bounds = boxCollider.bounds;
-        for (int i = 0; i < numberOfBombs; i++)
+    }
+
+    private void Update()
+    {
+        bounds = boxCollider.bounds;
+
+        while (numberOfBombs < maxBombs)
         {
             Vector3 randomPosition = new Vector3(
                 Random.Range(boxCollider.bounds.min.x, boxCollider.bounds.max.x),
@@ -34,8 +42,25 @@ public class MoveUpDownPrefab : MonoBehaviour
             );
             randomPosition.x += Random.Range(-0.5f, 0.5f);
             randomPosition.y += Random.Range(-0.5f, 0.5f);
-            Instantiate(prefab, randomPosition, Quaternion.identity);
+            GameObject instantiatedBomb = Instantiate(prefab, randomPosition, Quaternion.identity);
+            existingBombs.Add(instantiatedBomb);
+            numberOfBombs++;
         }
+
+        ResetPosition();
     }
 
+    private void ResetPosition()
+    {
+        for (int i = 0; i < existingBombs.Count; i++)
+        {
+            if (!bounds.Contains(existingBombs[i].transform.position))
+            {
+                Destroy(existingBombs[i]);
+                existingBombs.Remove(existingBombs[i]);
+                numberOfBombs--;
+                i--;
+            }
+        }
+    }
 }
