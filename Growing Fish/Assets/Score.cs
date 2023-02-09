6077a269311412be41cbfea3344b95f8da662a59
Zack <71916918+zakki-miilo@users.noreply.github.com>
2023-02-09 16:57:08 +1300

    Update scoreboard!

--- Growing Fish/Assets/Score.cs
+++ Growing Fish/Assets/Score.cs
@@ -5,18 +5,67 @@ using TMPro;
 
 public class Score : MonoBehaviour
 {
+
     public TMP_Text scoreText;
-    public PlayerInfo playerInfo;
+    public int score = 0;
+    public float timeInterval = 1.0f;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        score = 0;
+        UpdateScoreText();
+        StartCoroutine(AutoIncreaseScore());
     }
 
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = string.Format("{0:000000}", score);
+    }
+
+    IEnumerator AutoIncreaseScore()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(timeInterval);
+            AddScore(10);
+        }
+    }
+
+
+    /*public TMP_Text scoreText;
+    //public PlayerInfo playerInfo;
+    //like subway surfers
+    public int score = 0;
+    public float timeInterval = 1.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        score = 0;
+        UpdateScoreText();
+    }
+  
+    public void AddScore(int points)
+    {
+        score += points;
+        UpdateScoreText();
+    }
+
+    private void UpdateScoreText()
+    {
+        scoreText.text = string.Format("{0:000000}", score);
+    }
+    /*
     // Update is called once per frame
     void Update()
     {
         scoreText.text = "Score: " + (int)playerInfo.score;
-    }
+    }*/
 }
